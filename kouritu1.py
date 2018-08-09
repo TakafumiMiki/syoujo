bac255a164f28b800c46e9cff82d579c2fafd9ed
TakafumiMiki <karugami02468@gmail.com>
2018-08-10 04:33:47 +0900

    update syoujo prog

--- kouritu1.py
+++ kouritu1.py
@@ -3,7 +3,7 @@ import pandas as pd
 path = r"C:\Users\karug\Desktop\syoujo\kouhousien.csv"
 df = pd.read_csv(path,index_col=0)
 res = df.assign(
-    sum=df["zinryoku"] + df["zanyaku"] + df["haikyu"] + df["parts"]*3
+    sum=df["zinryoku"] + df["zanyaku"] #+ df["haikyu"] + df["parts"]*3
 )
 #res1 = 時間的最高効率
 res1 = df.assign(
---
+++ kouritu4.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+path = r"C:\Users\karug\Desktop\syoujo\kouhousien.csv"
+df = pd.read_csv(path,index_col=0)
+
+#res4 = 全素材最大数
+def allSys(num):
+    res4 = df.sort_values(num, ascending = False)
+    print(res4)
+
+def numchenge(getnum):
+    if getnum == "1":
+        getnum = "zinryoku"
+    elif getnum == "2":
+        getnum = "zanyaku"
+    elif getnum == "3":
+        getnum = "haikyu"
+    elif getnum == "4":
+        getnum = "parts"
+    allSys(getnum)
+#優先度の順を決める
+
+print("優先度の選択\n1:人力 2:弾薬 3:配給 4:パーツ")
+getnum = input()
+
+numchenge(getnum)
+print(getnum)
